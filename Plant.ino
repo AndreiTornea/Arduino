0601a704301fa9249ccdf8fc8c4980b30398dcc4
DacianWolf <tornea1999@gmail.com>
2018-02-05 10:36:23 +0100

    Update Plant.ino

--- Plant.ino
+++ Plant.ino
@@ -1,31 +1,31 @@
 #include <LiquidCrystal.h>
 
-//Digitali:
-int fan = 4;
-int light = 7;
-int pump = 2;
-int buzzer = 9;
-int ledred = 12;
-int ledgreen = 8;
-//Analogici:
-int soulPin = A0;
+//Digital:
+int Fan = 4;
+int Light = 7;
+int Pump = 2;
+int Buzzer = 9;
+int LedRed = 12;
+int LedGreen = 8;
+//Analog:
+int SoulPin = A0;
 int H2OPin = A1;
-int brightness = A2;
-float temp = A3;
-//Valori:
-int humidity_level;
+int Brightness = A2;
+float Temp = A3;
+//Values:
+int Humidity_level;
 int H2O_level;
-int light_level;
-int temp_level;
+int Light_level;
+int Temp_level;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(fan, OUTPUT);
-  pinMode(light, OUTPUT);
-  pinMode(pump, OUTPUT);
-  pinMode(ledgreen, OUTPUT);
-  pinMode(ledred, OUTPUT);
+  pinMode(Fan, OUTPUT);
+  pinMode(Light, OUTPUT);
+  pinMode(Pump, OUTPUT);
+  pinMode(LedGreen, OUTPUT);
+  pinMode(LedRed, OUTPUT);
   lcd.begin(16, 2);
   lcd.print("hello world!");
 }
@@ -33,53 +33,52 @@ void setup() {
 void loop() {
   lcd.setCursor(0, 1);
   lcd.print(millis()/1000);
-  humidity_level = analogRead(soulPin);
+  Humidity_level = analogRead(SoulPin);
   H2O_level = analogRead(H2OPin);
-  light_level = analogRead(brightness);
-  temp_level = analogRead(temp);
-  float voltage = temp_level * 5.0;
+  Light_level = analogRead(Brightness);
+  Temp_level = analogRead(Temp);
+  float voltage = Temp_level * 5.0;
   voltage /= 1024.0; 
-  float temperatureC = (voltage - 0.5) * 100 ;
-  Serial.println("Gradi:");Serial.println(temperatureC);
+  float TemperatureC = (voltage - 0.5) * 100 ;
+  Serial.println("Degrees:");Serial.println(TemperatureC);
 
   
-  if (humidity_level < 50){
-	digitalWrite(pump, HIGH);
+  if (Humidity_level < 50){
+	digitalWrite(Pump, HIGH);
 	delay(1000);
   }
   else {
-    digitalWrite(pump,LOW);
+    digitalWrite(Pump,LOW);
 	delay(1000);
   }
 
   if (H2O_level < 100){
-    digitalWrite(ledred, HIGH);
-    digitalWrite(ledgreen,LOW);
-	digitalWrite(pump,LOW);
+    digitalWrite(LedRed, HIGH);
+    digitalWrite(LedGreen,LOW);
+	digitalWrite(Pump,LOW);
 	delay(1000);
   }
   else {
-    digitalWrite(ledred,LOW);
-    digitalWrite(ledgreen, HIGH);
+    digitalWrite(LedRed,LOW);
+    digitalWrite(LedGreen, HIGH);
 	delay(1000);
   }
 
-  if (light_level < 50){
-    digitalWrite(light, LOW);
+  if (Light_level < 50){
+    digitalWrite(Light, LOW);
     delay(1000);
   }
-  if (light_level > 60){
-    digitalWrite(light,HIGH);
+  if (Light_level > 60){
+    digitalWrite(Light,HIGH);
     delay(1000);
   }
 
-  if(temperatureC > 28){
-    digitalWrite(fan,HIGH);
+  if(TemperatureC > 28){
+    digitalWrite(Fan,HIGH);
     delay(1000);
   }
-  if(temperatureC < 24){
-    digitalWrite(fan,LOW);
+  if(TemperatureC < 24){
+    digitalWrite(Fan,LOW);
     delay(1000);
   }
 }
-
